92d2ebb7e893df538812b272915cb5e4090aaf83
Albert Martin <albert.martin@acquia.com>
2013-06-19 10:32:35 -0400

    Tweaked regex for stricter matching.

--- contentscript.js
+++ contentscript.js
@@ -1,8 +1,8 @@
 /*
  * Albert Martin
  */
-var regex = /Ticket/;
-var myregex = /My.*Ticket/;
+var regex = /Ticket\ List.*\(\d+\)/;
+var myregex = /Ticket\ List.+My.+\(\d+\)/;
 
 // Test the text of the body element against our regular expression.
 if (regex.test(document.body.innerText)) {
