2b17ba360c9290ec7afd486e81703d1326ea0b7f
Albert Martin <albert.martin@acquia.com>
2013-07-16 19:51:42 -0400

    Fixed restore_options function not apply setting state to checkbox.

--- options.js
+++ options.js
@@ -14,11 +14,9 @@ function save_options() {
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-    var checked=false;
     chrome.storage.local.get('newWindowTickets',function(data){
-        checked=data.newWindowTickets;
+        document.getElementById("newWindowTickets").checked=data.newWindowTickets;
     });
-    document.getElementById("newWindowTickets").checked=localStorage["newWindowTickets"];
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
