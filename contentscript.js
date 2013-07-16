e0fc0f80fc59942505a4640b60fda0caa51ab964
Matt Lucasiewicz <Matthew.Lucasiewicz@acquia.com>
2013-07-16 15:05:59 -0400

    Only work if expiration field exists

--- contentscript.js
+++ contentscript.js
@@ -27,8 +27,6 @@ function secondsToTime(seconds){
     return out;
 }
 var regex = /Ticket\ List.*\(.*\)/;
-var myregex = /Ticket\ List.+My.+\(.*\)/;
-var slaregex = /Ticket\ List.+SLA.+Party.+\(.*\)/;
 if (regex.test(document.body.innerText)) {
     chrome.extension.sendRequest({}, function(response) {});
 
@@ -54,54 +52,56 @@ if (regex.test(document.body.innerText)) {
         tickets.push(arrayItem);
     });
 
-    // Add the extra column for Time to SLA
-    $("#lockedHeader tr").prepend('<td class="winColName" nowrap style="width:90px">Time to SLA</td>');
-    $("#tableContent thead tr").prepend('<td class="winColName" nowrap style="width:90px">Time to SLA</td>');
+    if(tickets.length > 0 && 'Expiry Timestamp' in tickets[0]){
+        // Add the extra column for Time to SLA
+        $("#lockedHeader tr").prepend('<td class="winColName" nowrap style="width:90px">Time to SLA</td>');
+        $("#tableContent thead tr").prepend('<td class="winColName" nowrap style="width:90px">Time to SLA</td>');
 
-    // Loop through each row of tickets
-    for (i = 0; i < tickets.length; i++) {
-        var timezone = tickets[i]['Expiry Timestamp'].slice(-3);
-        if(timezone == 'EDT'){
-            timezone = '-0400';
-        }
-
-        var expire = moment(tickets[i]['Expiry Timestamp'].slice(0,-3)+timezone, 'YYYY-MM-DD HH:mm ZZ');
-        var color = 'grey';
-        var sla = '';
-
-        // Colour the cell based on whether or not SLA was missed
-        var diff = expire.diff(moment(), "minutes");
-        if (diff >= 30) {
-          color = 'green';
-          sla = secondsToTime(expire.diff(moment(), "seconds"));
-        } else if (diff <= 0){
-          color = 'red';
-          sla = secondsToTime((-1*expire.diff(moment(), "seconds")))+ " ago";
-        } else {
-          color = 'yellow';
-          sla = secondsToTime(expire.diff(moment(), "seconds"));
-        }
-
-        // Style the SLA cell and print it out!
-        //$('#listRow'+i+' td:nth-child(12)').replaceWith('<td>'+ticket_updated+'</td>');
-        $('#listRow'+i).prepend('<td nowrap style="width:90px;background-color:'+color+'" class="slate">'+sla+'</td>');
-
-        //$('#listRow'+i).prepend('<td nowrap style="width:90px;background-color:'+color+'" class="slate">'+expire.format()+'</td>');
-        /*
-        // todo: fetch the ticket with AJAX and parse it to see if a response has been posted
-        // no need to display the "time till SLA" if it's already been ack'd
-        // bonus: maybe this changes to a "SLA met" or "SLA missed" state depending on the parsed timestamp of the first response?
-        if (myregex.test(document.body.innerText)) {
-            if (color=='green') {
-                sla = 'HIT';
-            } else {
-                sla = 'MISSED';
+        // Loop through each row of tickets
+        for (i = 0; i < tickets.length; i++) {
+            var timezone = tickets[i]['Expiry Timestamp'].slice(-3);
+            if(timezone == 'EDT'){
+                timezone = '-0400';
             }
-        } else {
-            // Change the SLA column to be a relative timestamp
-            sla = jQuery.timeago(moment(sla).format());
-        }*/
-        sla = null;
+
+            var expire = moment(tickets[i]['Expiry Timestamp'].slice(0,-3)+timezone, 'YYYY-MM-DD HH:mm ZZ');
+            var color = 'grey';
+            var sla = '';
+
+            // Colour the cell based on whether or not SLA was missed
+            var diff = expire.diff(moment(), "minutes");
+            if (diff >= 30) {
+                color = 'green';
+                sla = secondsToTime(expire.diff(moment(), "seconds"));
+            } else if (diff <= 0){
+                color = 'red';
+                sla = secondsToTime((-1*expire.diff(moment(), "seconds")))+ " ago";
+            } else {
+                color = 'yellow';
+                sla = secondsToTime(expire.diff(moment(), "seconds"));
+            }
+
+            // Style the SLA cell and print it out!
+            //$('#listRow'+i+' td:nth-child(12)').replaceWith('<td>'+ticket_updated+'</td>');
+            $('#listRow'+i).prepend('<td nowrap style="width:90px;background-color:'+color+'" class="slate">'+sla+'</td>');
+
+            //$('#listRow'+i).prepend('<td nowrap style="width:90px;background-color:'+color+'" class="slate">'+expire.format()+'</td>');
+            /*
+             // todo: fetch the ticket with AJAX and parse it to see if a response has been posted
+             // no need to display the "time till SLA" if it's already been ack'd
+             // bonus: maybe this changes to a "SLA met" or "SLA missed" state depending on the parsed timestamp of the first response?
+             if (myregex.test(document.body.innerText)) {
+             if (color=='green') {
+             sla = 'HIT';
+             } else {
+             sla = 'MISSED';
+             }
+             } else {
+             // Change the SLA column to be a relative timestamp
+             sla = jQuery.timeago(moment(sla).format());
+             }*/
+            sla = null;
+        }
     }
 }
 
