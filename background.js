18569b21c6331a431fa4ebd0657d47b608cd0470
Albert Martin <albert.martin@acquia.com>
2013-08-26 11:36:09 -0400

    Optimized site checking functionality.
    Added trusted hosts and changeInfo status checking to avoid calls from
    happening multiple times for each tab.

--- background.js
+++ background.js
@@ -2,20 +2,30 @@ chrome.browserAction.setBadgeBackgroundColor({ color: "#468847" });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   pageURL = tab.url;
-  host = pageURL.split('/')[2];
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://"+host+"/ACQUIA_MONITOR", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var status = xhr.responseText.slice(-8, -1);
-      if (status == "success") {
-        chrome.browserAction.setBadgeText({ text: "√", tabId: tab.id });
-        chrome.browserAction.setTitle({ title: "This site is hosted with Acquia.", tabId: tab.id });
-      } else {
-        chrome.browserAction.setTitle({ title: "Acquia SLA", tabId: tab.id });
+  if (pageURL !== undefined && changeInfo.status == "complete") {
+    host = pageURL.split('/')[2];
+
+    trustedHosts = [
+      'newtab',
+      's5.parature.com',
+    ];
+
+    if (trustedHosts.indexOf(host) == '-1') {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://"+host+"/ACQUIA_MONITOR", true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          var status = xhr.responseText.slice(-8, -1);
+          if (status == "success") {
+            chrome.browserAction.setBadgeText({ text: "√", tabId: tab.id });
+            chrome.browserAction.setTitle({ title: "This site is hosted with Acquia.", tabId: tab.id });
+          } else {
+            chrome.browserAction.setTitle({ title: "Acquia SLA", tabId: tab.id });
+          }
+        }
       }
+      xhr.send();
     }
   }
-  xhr.send();
 });
