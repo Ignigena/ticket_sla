5750196d078270eea623bbaf4432fad715e0c7a0
Albert Martin <albert.martin@acquia.com>
2013-09-03 21:47:41 -0400

    Added jQuery, changed function naming to camelcase to match JS conventions.

--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 // Saves options to localStorage.
-function save_options() {
     var select = document.getElementById("newWindowTickets");
     var checked = select.checked;
     chrome.storage.local.set({newWindowTickets: checked});
+function saveOptions() {
 
     // Update status to let user know options were saved.
     var status = document.getElementById("status");
@@ -13,10 +13,11 @@ function save_options() {
 }
 
 // Restores select box state to saved value from localStorage.
-function restore_options() {
+function restoreOptions() {
     chrome.storage.local.get('newWindowTickets',function(data){
         document.getElementById("newWindowTickets").checked=data.newWindowTickets;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('input').addEventListener('click', save_options);
+document.querySelector('input').addEventListener('click', save_options);document.addEventListener('DOMContentLoaded', restoreOptions);
+$('input').click(saveOptions);
