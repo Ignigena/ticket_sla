0c8f6aed1f9b1d0f455f1020adad7b791385b815
Albert Martin <albert.martin@acquia.com>
2013-11-14 14:13:48 -0500

    Added automated Escalated Advocacy queue notifications.

--- scripts/background.js
+++ scripts/background.js
@@ -63,3 +63,22 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   var toolFunction = toolActions[request.execute](request);
   $.when(toolFunction).done(function(response){ sendResponse(response); });
 });
+
+// Check the Escalated Advocacy ticket list every minute for new tickets.
+setInterval(function() {
+  $.ajax('https://s5.parature.com/ics/tt/ticketlist.asp?filter_queue=3439').done(function(data) {
+    $('tbody tr.gridRow', data).each(function() {
+      var ticketLink = $('td a.link2', this).first().attr('href').split('ticket_id=')[1];
+      var ticketNumber = $('td a.link2', this).first().text().split('-')[1];
+      
+      var notify = webkitNotifications.createNotification(
+        'slatoolbar.png',
+        'Escalated Advocacy',
+        'Ticket '+ticketNumber+' has been placed in Escalated Advocacy.'
+      );
+      notify.onclick = function(x) { chrome.tabs.create({ url: "https://s5.parature.com/ics/tt/ticketDetail.asp?ticket_id="+ticketLink }); };
+      notify.show();
+      setTimeout(function(){ notify.cancel(); }, 10000);
+    });
+  });
+}, 60000);
