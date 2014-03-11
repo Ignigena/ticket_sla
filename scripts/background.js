54edf743f2da15ef195ef64a863f8708e7537bf0
Albert Martin <albert.martin@acquia.com>
2014-03-11 10:07:06 -0400

    Removed legacy Parature scripts.

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,6 @@
 var trustedHosts = [
   'newtab',
   'extensions',
-  's5.parature.com',
 ];
 
 chrome.browserAction.setBadgeBackgroundColor({ color: "#468847" });
@@ -63,22 +62,3 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   var toolFunction = toolActions[request.execute](request);
   $.when(toolFunction).done(function(response){ sendResponse(response); });
 });
-
-// Check the Escalated Advocacy ticket list every 5 minutes for outstanding tickets.
-setInterval(function() {
-  $.ajax('https://s5.parature.com/ics/tt/ticketlist.asp?filter_queue=3439').done(function(data) {
-    $('tbody tr.gridRow', data).each(function() {
-      var ticketLink = $('td a.link2', this).first().attr('href').split('ticket_id=')[1];
-      var ticketNumber = $('td a.link2', this).first().text().split('-')[1];
-      
-      var notify = webkitNotifications.createNotification(
-        'slatoolbar.png',
-        'Escalated Advocacy',
-        'Ticket '+ticketNumber+' has been placed in Escalated Advocacy.'
-      );
-      notify.onclick = function(x) { chrome.tabs.create({ url: "https://s5.parature.com/ics/tt/ticketDetail.asp?ticket_id="+ticketLink }); };
-      notify.show();
-      setTimeout(function(){ notify.cancel(); }, 10000);
-    });
-  });
-}, 300000);
