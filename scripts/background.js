17a19d51b85a1bb778fdadf2f58bbbb50c5835c1
Albert Martin <albert.martin@acquia.com>
2014-04-02 17:17:28 -0400

    Fixed Javascript errors and minor code refactor.

--- scripts/background.js
+++ scripts/background.js
@@ -1,103 +1,101 @@
 var trustedHosts = [
-  'newtab',
-  'extensions',
+    'newtab',
+    'extensions'
 ];
 
 chrome.browserAction.setBadgeBackgroundColor({ color: "#468847" });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.url && changeInfo.url.indexOf("chrome://") != 0) {
-    checkZendeskTicketTab(tab, changeInfo.url);
-  }
-
-  pageURL = tab.url;
-
-  if (pageURL !== undefined && changeInfo.status == "complete") {
-    var host = pageURL.split('/')[2];
-
-    if (trustedHosts.indexOf(host) == '-1') {
-      chrome.storage.local.get('hostCache',function(data){
-          var cachedHosts = {};
-          var hostedStatus = false;
-          var currentTime = new Date().getTime();
-
-          if (data.hostCache) {
-            cachedHosts = JSON.parse(data.hostCache);
-          }
-
-          if (!cachedHosts[host] || currentTime >= cachedHosts[host]['cached']+86400) {
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://"+host+"/ACQUIA_MONITOR", true);
-            xhr.onreadystatechange = function() {
-              if (xhr.readyState == 4) {
-                var status = xhr.responseText.slice(-8, -1);
-                if (status == "success") {
-                  hostedStatus = true;
-                } else {
-                  hostedStatus = false;
-                }
-                cachedHosts[host] = { hosted: hostedStatus, cached: currentTime }
-                chrome.storage.local.set({hostCache: JSON.stringify(cachedHosts)});
-
-                updateBrowserAction(hostedStatus);
-              }
-            }
-            xhr.send();
-          } else {
-            hostedStatus = cachedHosts[host]['hosted'];
-
-            updateBrowserAction(hostedStatus);
-          }
-      });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.url && changeInfo.url.indexOf("chrome://") != 0) {
+        checkZendeskTicketTab(tab, changeInfo.url);
     }
-  }
+
+    var pageURL = tab.url;
+
+    if (pageURL !== undefined && changeInfo.status == "complete") {
+        var host = pageURL.split('/')[2];
+
+        if (trustedHosts.indexOf(host) == '-1') {
+            chrome.storage.local.get('hostCache', function (data) {
+                var cachedHosts = {};
+                var hostedStatus = false;
+                var currentTime = new Date().getTime();
+
+                if (data.hostCache) {
+                    cachedHosts = JSON.parse(data.hostCache);
+                }
+
+                if (!cachedHosts[host] || currentTime >= cachedHosts[host]['cached'] + 86400) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "http://" + host + "/ACQUIA_MONITOR", true);
+                    xhr.onreadystatechange = function () {
+                        if (xhr.readyState == 4) {
+                            var status = xhr.responseText.slice(-8, -1);
+                            hostedStatus = status == "success";
+                            cachedHosts[host] = { hosted: hostedStatus, cached: currentTime };
+                            chrome.storage.local.set({hostCache: JSON.stringify(cachedHosts)});
+
+                            updateBrowserAction(hostedStatus, tab);
+                        }
+                    };
+                    xhr.send();
+                } else {
+                    hostedStatus = cachedHosts[host]['hosted'];
+
+                    updateBrowserAction(hostedStatus, tab);
+                }
+            });
+        }
+    }
 });
 
-chrome.tabs.onCreated.addListener(function(newTab) {
-  if (newTab.url) {
-    checkZendeskTicketTab(newTab, newTab.url);
-  }
+chrome.tabs.onCreated.addListener(function (newTab) {
+    if (newTab.url) {
+        checkZendeskTicketTab(newTab, newTab.url);
+    }
 });
 
-function updateBrowserAction(hostedStatus) {
-  if (hostedStatus) {
-    chrome.browserAction.setIcon({ path: 'toolbar-acquia.png', tabId: tab.id });
-    chrome.browserAction.setTitle({ title: "This site is hosted with Acquia.", tabId: tab.id });
-  } else {
-    chrome.browserAction.setTitle({ title: "Support Tools", tabId: tab.id });
-  }
+function updateBrowserAction(hostedStatus, tab) {
+    if (hostedStatus) {
+        chrome.browserAction.setIcon({ path: 'toolbar-acquia.png', tabId: tab.id });
+        chrome.browserAction.setTitle({ title: "This site is hosted with Acquia.", tabId: tab.id });
+    } else {
+        chrome.browserAction.setTitle({ title: "Support Tools", tabId: tab.id });
+    }
 }
 
 function checkZendeskTicketTab(tab, tabUrl) {
-  if (tabUrl.indexOf('https://acquia.zendesk.com/agent/#') == 0) {
-    chrome.tabs.query({ url: 'https://acquia.zendesk.com/*' }, function (tabs) {
-      if (tabs && tabs[0]) {
-        if (tabs[0].id != tab.id) {
-          // Only merge tabs if the current tab and the first matched tab aren't the same.
-          mergeNewTabWithOriginal(tabs[0], tab);
-        }
-        else if (tabs.length >= 2 && tabs[1] != tab) {
-          // Allow for merging if the current tab is the first matched tab but another exists.
-          mergeNewTabWithOriginal(tabs[1], tab);
-        }
-      }
-    });
-  }
+    if (tabUrl.indexOf('https://acquia.zendesk.com/agent/#') == 0) {
+        chrome.tabs.query({ url: 'https://acquia.zendesk.com/*' }, function (tabs) {
+            if (tabs && tabs[0]) {
+                if (tabs[0].id != tab.id) {
+                    // Only merge tabs if the current tab and the first matched tab aren't the same.
+                    mergeNewTabWithOriginal(tabs[0], tab);
+                }
+                else if (tabs.length >= 2 && tabs[1] != tab) {
+                    // Allow for merging if the current tab is the first matched tab but another exists.
+                    mergeNewTabWithOriginal(tabs[1], tab);
+                }
+            }
+        });
+    }
 }
 
 function mergeNewTabWithOriginal(originalTab, newTab) {
-  if (originalTab.url != newTab.url) {
-    chrome.tabs.update(originalTab.id, { url: newTab.url, highlighted: true });
-  } else {
-    chrome.tabs.update(originalTab.id, { highlighted: true });
-  }
+    if (originalTab.url != newTab.url) {
+        chrome.tabs.update(originalTab.id, { url: newTab.url, highlighted: true });
+    } else {
+        chrome.tabs.update(originalTab.id, { highlighted: true });
+    }
 
-  if (originalTab.id != newTab.id) {
-    chrome.tabs.remove(newTab.id);
-  }
+    if (originalTab.id != newTab.id) {
+        chrome.tabs.remove(newTab.id);
+    }
 }
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  var toolFunction = toolActions[request.execute](request);
-  $.when(toolFunction).done(function(response){ sendResponse(response); });
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    var toolFunction = toolActions[request.execute](request);
+    $.when(toolFunction).done(function (response) {
+        sendResponse(response);
+    });
 });
--- scripts/tool_actions.js
+++ scripts/tool_actions.js
@@ -1,74 +1,44 @@
 var toolActions = {};
 
-toolActions.notification = function notifications(request) {
-  // Only display popup alerts if the Chrome Notifications option is set.
-  chrome.storage.local.get('chromeNotifications',function(data){
-    if(data.chromeNotifications){
-      // Set up the alert parameters.
-      var notify = webkitNotifications.createNotification(
-        'slatoolbar.png',
-        request.title,
-        request.msg
-      );
+toolActions.getAcquiaMonitor = function getAcquiaMonitor() {
+    var defer = new $.Deferred();
 
-      if (request.mode == 'load') {
-        // Clicking on the alert will load the ticket detail view.
-        // Used in a ticket list when one ticket creates a new alert.
-        notify.onclick = function(x) { chrome.tabs.create({ url: "https://s5.parature.com/ics/tt/ticketDetail.asp?ticket_id="+request.ticket }); };
-      } else {
-        // Default behaviour, focus the window that spawned the alert.
-        // This is used when composing a message where it would be disruptive to load the same ticket in a new window.
-        notify.onclick = function(x) { chrome.tabs.update(sender.tab.id, {active: true}); this.cancel(); };
-      }
+    chrome.tabs.getSelected(function (tab) {
+        var host = tab.url.split('/')[2];
 
-      // Show the alert.
-      notify.show();
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://" + host + "/ACQUIA_MONITOR", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var monitor = {};
+                var response = xhr.responseText.split("\n");
+                $.each(response, function (index, item) {
+                    var lineDecode = item.split("=");
+                    monitor[lineDecode[0]] = lineDecode[1];
+                });
+                defer.resolve(monitor);
+            }
+        };
+        xhr.send();
+    });
 
-      // Automatically hide the alert after 10 seconds.
-      setTimeout(function(){ notify.cancel(); }, 10000);
-    }
-  });
-}
-
-toolActions.getAcquiaMonitor = function getAcquiaMonitor(request) {
-  var defer = new $.Deferred();
-
-  chrome.tabs.getSelected(function(tab){
-    var host = tab.url.split('/')[2];
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://"+host+"/ACQUIA_MONITOR", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        var monitor = {};
-        var response = xhr.responseText.split("\n");
-        $.each(response, function(index, item) {
-          var lineDecode = item.split("=");
-          monitor[lineDecode[0]] = lineDecode[1];
-        });
-        defer.resolve(monitor);
-      }
-    }
-    xhr.send();
-  });
-
-  return defer.promise();
-}
+    return defer.promise();
+};
 
 toolActions.pinToWeb = function pinToWeb(request) {
-  var current = $.cookie('ah_app_server');
+    var current = $.cookie('ah_app_server');
 
-  if (current) {
-      if (request.pinTo) {
-        $.removeCookie('ah_app_server');
-      }
-      return('unpinned:'+current+':'+request.pinTo);
-  } else {
-      if (request.pinTo) {
-        var tmp = request.pinTo.split('.');
-        console.log(tmp);
-        $.cookie('ah_app_server', tmp[0]);
-      }
-      return('pinned:'+tmp[0]+':'+request.pinTo);
-  }
-}
+    if (current) {
+        if (request.pinTo) {
+            $.removeCookie('ah_app_server');
+        }
+        return('unpinned:' + current + ':' + request.pinTo);
+    } else {
+        if (request.pinTo) {
+            var tmp = request.pinTo.split('.');
+            console.log(tmp);
+            $.cookie('ah_app_server', tmp[0]);
+        }
+        return('pinned:' + tmp[0] + ':' + request.pinTo);
+    }
+};
