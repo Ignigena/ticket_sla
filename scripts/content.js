974f5d74aea87834570756dff0c20489dcffa82a
Albert Martin <albert.martin@acquia.com>
2014-05-28 10:58:31 -0400

    Implemented DOM modification of ZD "add" menu.

---
+++ scripts/content.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $('#branding_header li.tab.add span#i18n-23').text('recent tickets');
+  $('div.popover.add.bottom h3, div.popover.add.bottom .popover-content > button').hide();
+});
