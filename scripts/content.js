d9c3780c755261c6e3c4f966f91bd728bb6ae5d3
Albert Martin <albert.martin@acquia.com>
2014-05-29 12:32:23 -0400

    Removed "take it" shortcut to avoid confusion.

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
   $('#branding_header li.tab.add span#i18n-23').text('recent tickets');
   $('div.popover.add.bottom h3, div.popover.add.bottom .popover-content > button').hide();
+  $('.assignee_widget label div.link_light').remove();
 });
