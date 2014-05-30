00b57f62ee3bfcc4bd346840db53a6c33047ba4e
Albert Martin <albert.martin@acquia.com>
2014-05-30 10:48:33 -0400

    Listen to changes in the ZD tabs header.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('#branding_header li.tab.add span#i18n-23').text('recent tickets');
   $('div.popover.add.bottom h3, div.popover.add.bottom .popover-content > button').hide();
-  $('.assignee_widget label div.link_light').remove();
+
+  $('#branding_header').bind('DOMSubtreeModified', function() {
+    $('.assignee_widget label div.link_light').remove();
+  });
 });
