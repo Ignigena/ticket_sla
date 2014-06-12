2c574bdd9302be355a76ae5f19f20a8a234852fc
Albert Martin <albert.martin@acquia.com>
2014-06-12 14:58:23 -0400

    Fixed class names targeted for UI modifications.

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#branding_header li.tab.add span#i18n-23').text('recent tickets');
-  $('div.popover.add.bottom h3, div.popover.add.bottom .popover-content > button').hide();
+  $('div.popover.add.bottom h3, div.popover .links-section, div.popover.add.bottom .popover-content > button').hide();
+  $('.assignee_widget label div.link_light').remove();
 
   $('#branding_header').bind('DOMSubtreeModified', function() {
     $('.assignee_widget label div.link_light').remove();
